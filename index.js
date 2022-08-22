79e24fd34258161f03e92b8387be223646823346
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-22 11:23:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const input = document.getElementById("button");
+  
 function addingEventListener() {
+  input.addEventListener("click", function (event) {
+    alert("I was clicked!");
+  });
 }
+addingEventListener()
